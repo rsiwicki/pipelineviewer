46a5dd2d6d99a5eea785e065420ba216c765281c
rob <robert.siwicki@googlemail.com>
2022-01-24 23:04:53 +0000

    handle the pipe broken

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
 use std::env;
-use std::io::{self, Read, Write};
+use std::io::{self, ErrorKind, Result, Read, Write};
 
 const CHUNK_SIZE_KB: usize = 16 * 1024;
 
-fn main() {
-
+fn main() -> Result<()> {
     let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
     let mut total_bytes = 0;
+    let mut buffer = [0; CHUNK_SIZE_KB];
     loop {
-        let mut buffer = [0; CHUNK_SIZE_KB];
         let num_read = match io::stdin().read(&mut buffer) {
             Ok(0) => break,
             Ok(x) => x,
             Err(_) => break,
         };
         total_bytes += num_read;
-        io::stdout().write_all(&buffer[..num_read]).unwrap();
+        if !silent {
+            eprint!("\r{}", total_bytes);
+        }
+        if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+            return Err(e);
+        }
     }
 
     if !silent {
-        eprintln!("num_read: {}", total_bytes);
+        eprintln!("\r{}", total_bytes);
     }
+
+    Ok(())
 }
