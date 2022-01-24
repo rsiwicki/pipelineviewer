9f63a99e181ebcc87cb8813515b9564ff44ee92f
rob <robert.siwicki@googlemail.com>
2022-01-24 22:27:43 +0000

    playing format with

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,21 @@ use std::io::{self, Read, Write};
 const CHUNK_SIZE_KB: usize = 16 * 1024;
 
 fn main() {
-    let silent = env::var("PV_SILENT").unwrap_or(String::new()).len() > 0;
 
+    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
     let mut total_bytes = 0;
     loop {
         let mut buffer = [0; CHUNK_SIZE_KB];
         let num_read = match io::stdin().read(&mut buffer) {
             Ok(0) => break,
             Ok(x) => x,
-            Err(_) => break
+            Err(_) => break,
         };
         total_bytes += num_read;
         io::stdout().write_all(&buffer[..num_read]).unwrap();
     }
 
-    if (!silent) {
+    if !silent {
         eprintln!("num_read: {}", total_bytes);
     }
-
 }
